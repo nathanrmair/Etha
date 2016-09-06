bcaaf04077b071218c141ac0b3494f94cf861567
Mair <40170722@live.napier.ac.uk>
2016-09-06 16:01:28 +0100

    This is a message

--- app/views.py
+++ app/views.py
@@ -1,4 +1,11 @@
 from app import app
+
+import json
+
 @app.route('/')
 def homepage():
     return 'Hello world'
+
+@app.route('/register')
+def homepage2():
+    return json.dumps(["status":"OK"])
