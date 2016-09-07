2569d462df31925f7a3ef547045b4fee1616b7b8
Nathan Mair <40170722@live.napier.ac.uk>
2016-09-07 16:11:05 +0100

    Added update data handling

--- app/views.py
+++ app/views.py
@@ -2,7 +2,10 @@ from app import app, db
 from models import Driver, Passenger
 
 import json
+import math
 from flask import request
+
+DEGREES_TO_RADIANS = math.pi/180.0
 
 @app.route('/')
 def homepage():
@@ -33,11 +36,24 @@ def passenger():
 
 @app.route('/update',methods=['POST'])
 def update():
-    return json.dumps(
-	    {
-	        "status": "OK"
-	    }
-    )
+    passengers = Passenger.query.all()
+    passengers_list = [x for x in passengers
+            if distance_on_unit_sphere(x.src_lat,x.src_long,x.dest_lat,x.dest_long) < 10]
+    y = [{'src_lat': x.src_lat, 'src_long': x.src_long, 'dest_lat': x.dest_lat, 'dest_long': x.dest_long}
+            for x in passengers_list]
+    return json.dumps(y)
 
+def distance_on_unit_sphere(lat1, long1, lat2, long2):
+        # Taken from http://www.johndcook.com/blog/python_longitude_latitude/
+        phi1 = (90.0 - lat1) * DEGREES_TO_RADIANS
+        phi2 = (90.0 - lat2) * DEGREES_TO_RADIANS
 
+        theta1 = long1 * DEGREES_TO_RADIANS
+        theta2 = long2 * DEGREES_TO_RADIANS
+
+        cos = (math.sin(phi1) * math.sin(phi2) * math.cos(theta1 - theta2)
+                + math.cos(phi1) * math.cos(phi2))
+        arc = math.acos(cos)
+
+        return arc * 6373 # Times 6373 for km
 
