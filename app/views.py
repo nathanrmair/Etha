930a809a33169d745a9a9779dde2dee69f5576cb
Nathan Mair <40170722@live.napier.ac.uk>
2016-09-07 14:37:37 +0100

    Added passenger source and destionation handling

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
-from app import app
+from app import app, db
+from models import Driver, Passenger
 
 import json
 from flask import request
@@ -17,7 +18,13 @@ def register():
 
 @app.route('/passenger',methods=['POST'])
 def passenger():
-    
+    d=json.loads(request.data.decode('utf-8'))
+    dest = (d['dest']['lat'], d['dest']['lng'])
+    src = (d['src']['lat'],d['src']['lng'])
+    p = Passenger(src[0],src[1],
+            dest[0],dest[1])
+    db.session.add(p)
+    db.session.commit()
     return json.dumps(
 	    {
 	        "status": "OK"
