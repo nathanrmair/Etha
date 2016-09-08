4778ddb92204baa150c6310fe60d5a1971c5018a
hintofbasil <hintofbasil@users.noreply.github.com>
2016-09-08 09:44:55 +0100

    Added spoof_passenger option to manage.py

--- app/manage.py
+++ app/manage.py
@@ -1,12 +1,14 @@
 #!../env/bin/python
 from app import app, db
+from models import Passenger
 
 import sys
 
-USAGE = """Usage: manager.py function
+USAGE = """Usage: manager.py function [options]
 
 ---- Valid functions -----
 create_db
+spoof_passenger src_lat src_long dest_lat dest_long
 """
 
 class Manager:
@@ -20,15 +22,26 @@ class Manager:
     def process(self):
         if len(self.args) < 2:
             print(USAGE)
-            return
-        if self.args[1] == 'create_db':
+        elif self.args[1] == 'create_db':
             self.create_db()
+        elif self.args[1] == 'spoof_passenger':
+            self.spoof_passenger()
+        else:
+            print(USAGE)
 
     def close(self):
         self.app_context.pop()
 
     def create_db(self):
         db.create_all()
+
+    def spoof_passenger(self):
+        if len(self.args) < 6:
+            print(USAGE)
+            return
+        p = Passenger(*self.args[2:])
+        db.session.add(p)
+        db.session.commit()
 
 if __name__=='__main__':
     manager = Manager(sys.argv)
